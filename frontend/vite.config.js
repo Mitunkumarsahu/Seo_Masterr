9b6e610d4eb16ad7b94183fb1d341e771b0fb163
Mitunkumarsahu <mitunkumarsahu@gmail.com>
2025-07-20 23:30:02 +0530

    docker file of frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Listen on 0.0.0.0
-    port: 5173, // Optional, default is 5173
+    host: true,
+    port: 5173,
   },
   optimizeDeps: {
     include: ['@mui/material', '@mui/icons-material'],
