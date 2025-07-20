aaa6292c64c53056a5f07ab686698dafa7443df9
Mitunkumarsahu <mitunkumarsahu@gmail.com>
2025-07-20 23:09:48 +0530

    vite config and package.json chanegs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,13 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Listen on 0.0.0.0
+    port: 5173, // Optional, default is 5173
+  },
   optimizeDeps: {
     include: ['@mui/material', '@mui/icons-material'],
-  }
+  },
 })
