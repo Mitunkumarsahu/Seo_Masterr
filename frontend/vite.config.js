9b622972a7ade990cd196e6a5e75b89d32f6369e
Abinash Dash <abinash.dash@hyscaler.com>
2025-07-23 23:22:40 +0530

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: 'all'
+    allowedHosts: ['seomasterr.com']
   },
   optimizeDeps: {
     include: ['@mui/material', '@mui/icons-material'],
