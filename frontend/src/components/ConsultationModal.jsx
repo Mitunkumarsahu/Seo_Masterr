02724ffbe8762033ed6150f8db4c9ddbcb01435b
Mitunkumarsahu <mitunkumarsahu@gmail.com>
2026-02-14 20:17:58 +0530

    minor modification

--- frontend/src/components/ConsultationModal.jsx
+++ frontend/src/components/ConsultationModal.jsx
@@ -26,7 +26,6 @@ const ConsultationModal = ({ open, onClose }) => {
             <Fade in={open}>
                 <Box
                     sx={{
-                        position: "absolute",
                         top: "50%",
                         left: "50%",
                         transform: "translate(-50%, -50%)",
